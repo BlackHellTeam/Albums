950076e30d26ccc6cbac4090b31de7dd922a033e
Liang.Michael <xzc2116@gmail.com>
2015-01-15 20:58:51 +0800

    简要实现Instagram数据库类

--- app/src/main/java/com/liang/albums/db/InstagramProvider.java
+++ app/src/main/java/com/liang/albums/db/InstagramProvider.java
@@ -4,11 +4,19 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 /**
  * Created by liang on 15/1/14.
  */
-public class InstagramProvider extends ContentProvider {
+public class InstagramProvider extends BaseSocialProvider {
+
+    private static final String TAG = "InstagramProvider";
+
+    public final static String TABLE_NAME = "instagram";
+    public final static String AUTHORITY = "com.liang.albums.provider.instagram";
+    public final static Uri CONTENT_URI = Uri.parse("content://"+AUTHORITY+"/"+TABLE_NAME);
+
     @Override
     public boolean onCreate() {
         return false;
@@ -16,26 +24,27 @@ public class InstagramProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        return super.query(uri,projection,selection,selectionArgs,sortOrder);
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        return super.getType(uri);
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        return super.insert(uri, values);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        return super.delete(uri, selection, selectionArgs);
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        return super.update(uri, values, selection, selectionArgs);
     }
+
 }
