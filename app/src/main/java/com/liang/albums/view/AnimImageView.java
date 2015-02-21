0a8e79143fa13b81efecb6abc80ece581e6a3304
Liang.Michael <xzc2116@gmail.com>
2015-02-21 22:48:59 +0800

    use set bitmap method

--- app/src/main/java/com/liang/albums/view/AnimImageView.java
+++ app/src/main/java/com/liang/albums/view/AnimImageView.java
@@ -6,13 +6,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Handler;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
-
-import com.liang.albums.R;
 
 /**
  * Created by liang on 15/2/17.
@@ -21,8 +16,8 @@ public class AnimImageView extends View {
 
     private static String TAG = "AnimImageView";
 
+//    private Bitmap mBitmap;
     private Bitmap mBitmap;
-    private Bitmap dstbmp;
     private Paint mPaint;
 
     private int xPivot = 0;
@@ -34,42 +29,33 @@ public class AnimImageView extends View {
     public AnimImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mBitmap = ((BitmapDrawable)getResources().getDrawable(R.drawable.csy)).getBitmap();
+        //mBitmap = ((BitmapDrawable)getResources().getDrawable(R.drawable.csy)).getBitmap();
 
     }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        new Thread(mAnimRunnable, "AnimThread").start();
+//        new Thread(mAnimRunnable, "AnimThread").start();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // 定义矩阵对象
-        Matrix matrix = new Matrix();
-        if(mBitmap.getWidth()>mBitmap.getHeight()) {
-            // 缩放原图
-            matrix.postScale(getWidth()/(float)mBitmap.getWidth(), getWidth()/(float)mBitmap.getWidth());
-        }else{
-            matrix.postScale(getHeight()/(float)mBitmap.getHeight(), getHeight()/(float)mBitmap.getHeight());
-        }
-        //bmp.getWidth(), bmp.getHeight()分别表示缩放后的位图宽高
-        dstbmp = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(), mBitmap.getHeight(),
-                matrix, true);
+
+
 
         mPaint.setColor(Color.YELLOW);
         canvas.drawRect(0, 0, getWidth(), getHeight(), mPaint);
         mPaint.setColor(Color.BLUE);
         mPaint.setTextSize(20);         //canvas.drawRect(0, 0, getWidth(), getHeight(), mPaint);
         //canvas.drawText("Hello View", 0, getHeight() / 2, mPaint);
-        canvas.drawBitmap(dstbmp, xPivot, yPivot, mPaint);
-
-        Log.d(TAG, "mBitmap height = "+mBitmap.getHeight()+" width = "+mBitmap.getWidth());
-        Log.d(TAG, "dstbmp  height = "+dstbmp.getHeight()+" width = "+dstbmp.getWidth());
-        Log.d(TAG, "canvas  height = "+canvas.getHeight()+" width = "+canvas.getWidth());
-        Log.d(TAG, "view    height = "+getHeight()+" width = "+getWidth());
+        canvas.drawBitmap(mBitmap, xPivot, yPivot, mPaint);
+//
+//        Log.d(TAG, "mBitmap height = "+mBitmap.getHeight()+" width = "+mBitmap.getWidth());
+//        Log.d(TAG, "mBitmap height = "+mBitmap.getHeight()+" width = "+mBitmap.getWidth());
+//        Log.d(TAG, "canvas  height = "+canvas.getHeight()+" width = "+canvas.getWidth());
+//        Log.d(TAG, "view    height = "+getHeight()+" width = "+getWidth());
 
     }
 
@@ -83,16 +69,16 @@ public class AnimImageView extends View {
             }
             while(!Thread.currentThread().isInterrupted()){
                 try {
-                    Thread.sleep(200);
+                    Thread.sleep(300);
                 } catch (Exception e) {
                     Thread.currentThread().interrupt();
                 }
 
-                if(mBitmap.getWidth()>mBitmap.getHeight()) {
+                if(mBitmap.getWidth()> mBitmap.getHeight()) {
                     // 缩放原图
                     if(mDirection==1){
                         yPivot--;
-                        if(yPivot <= getHeight()-dstbmp.getHeight()){
+                        if(yPivot <= getHeight()- mBitmap.getHeight()){
                             mDirection = 0;
                         }
                     }else {
@@ -104,7 +90,7 @@ public class AnimImageView extends View {
                 }else{
                     if(mDirection==1){
                         xPivot--;
-                        if(xPivot <= getWidth()-dstbmp.getWidth()){
+                        if(xPivot <= getWidth()- mBitmap.getWidth()){
                             mDirection = 0;
                         }
                     }else {
@@ -120,5 +106,26 @@ public class AnimImageView extends View {
             }
         }
     };
+
+    public void setBitmap(Bitmap bitmap){
+        Bitmap srcBitmap = bitmap;
+
+        if(srcBitmap == null) return;
+
+        // 定义矩阵对象
+        Matrix matrix = new Matrix();
+        if(srcBitmap.getWidth()>srcBitmap.getHeight()) {
+            // 缩放原图
+            matrix.postScale(getWidth()/(float)srcBitmap.getWidth(), getWidth()/(float)srcBitmap.getWidth());
+        }else{
+            matrix.postScale(getHeight()/(float)srcBitmap.getHeight(), getHeight()/(float)srcBitmap.getHeight());
+        }
+        //bmp.getWidth(), bmp.getHeight()分别表示缩放后的位图宽高
+        mBitmap = Bitmap.createBitmap(srcBitmap, 0, 0, srcBitmap.getWidth(), srcBitmap.getHeight(),
+                matrix, true);
+
+        invalidate();
+        new Thread(mAnimRunnable, "AnimThread").start();
+    }
 
 }
